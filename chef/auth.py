13445a92ecd9045f820069ce1d1cd6d1a95c002e
Noah Kantrowitz <noah@coderanger.net>
2010-11-26 19:36:30 -0800

    Working implementation of the Opscode authentication protocol.

---
+++ chef/auth.py
@@ -0,0 +1,78 @@
+import base64
+import datetime
+import hashlib
+import re
+
+def _ruby_b64encode(value):
+    """The Ruby function Base64.encode64 automatically breaks things up
+    into 60-character chunks.
+    """
+    b64 = base64.b64encode(value)
+    for i in xrange(0, len(b64), 60):
+        yield b64[i:i+60]
+
+def ruby_b64encode(value):
+    return '\n'.join(_ruby_b64encode(value))
+
+def sha1_base64(value):
+    """An implementation of Mixlib::Authentication::Digester."""
+    return ruby_b64encode(hashlib.sha1(value).digest())
+
+class UTC(datetime.tzinfo):
+    """UTC timezone stub."""
+    
+    ZERO = datetime.timedelta(0)
+
+    def utcoffset(self, dt):
+        return self.ZERO
+
+    def tzname(self, dt):
+        return 'UTC'
+
+    def dst(self, dt):
+        return self.ZERO
+
+utc = UTC()
+
+def canonical_time(timestamp):
+    if timestamp.tzinfo is not None:
+        timestamp = timestamp.astimezone(utc).replace(tzinfo=None)
+    return timestamp.replace(microsecond=0).isoformat() + 'Z'
+
+canonical_path_regex = re.compile(r'/+')
+def canonical_path(path):
+    path = canonical_path_regex.sub('/', path)
+    if len(path) > 1:
+        path = path.rstrip('/')
+    return path
+
+def canonical_request(http_method, path, hashed_body, timestamp, user_id):
+    hashed_path = sha1_base64(path)
+    return ('Method:%(http_method)s\n'
+            'Hashed Path:%(hashed_path)s\n'
+            'X-Ops-Content-Hash:%(hashed_body)s\n'
+            'X-Ops-Timestamp:%(timestamp)s\n'
+            'X-Ops-UserId:%(user_id)s' % vars())
+
+def sign_request(key, http_method, path, body, host, timestamp, user_id):
+    """Generate the needed headers for the Opscode authentication protocol."""
+    # Canonicalize request parameters
+    http_method = http_method.upper()
+    path = canonical_path(path)
+    timestamp = canonical_time(timestamp)
+    hashed_body = sha1_base64(body or '')
+    
+    # Simple headers
+    headers = {
+        'X-Ops-Sign': 'version=1.0',
+        'X-Ops-Userid': user_id,
+        'X-Ops-Timestamp': timestamp,
+        'X-Ops-Content-Hash': hashed_body,
+    }
+    
+    # Create RSA signature
+    req = canonical_request(http_method, path, hashed_body, timestamp, user_id)
+    sig = _ruby_b64encode(key.encrypt(req))
+    for i, line in enumerate(sig):
+        headers['X-Ops-Authorization-%s'%(i+1)] = line
+    return headers
---
+++ chef/rsa.py
@@ -0,0 +1,82 @@
+from ctypes import *
+
+_eay = CDLL('libeay32.dll')
+
+class SSLError(Exception):
+    """An error in OpenSSL."""
+
+# BIO *BIO_new_mem_buf(void *buf, int len);
+BIO_new_mem_buf = _eay.BIO_new_mem_buf
+BIO_new_mem_buf.argtypes = [c_void_p, c_int,]
+BIO_new_mem_buf.restype = c_void_p
+
+# int    BIO_free(BIO *a)
+BIO_free = _eay.BIO_free
+BIO_free.argtypes = [c_void_p]
+BIO_free.restype = c_int
+def BIO_free_errcheck(result, func, arguments):
+    if result == 0:
+        raise SSLError('Unable to free BIO')
+BIO_free.errcheck = BIO_free_errcheck
+
+#RSA *PEM_read_bio_RSAPrivateKey(BIO *bp, RSA **x,
+#                                        pem_password_cb *cb, void *u);
+PEM_read_bio_RSAPrivateKey = _eay.PEM_read_bio_RSAPrivateKey
+PEM_read_bio_RSAPrivateKey.argtypes = [c_void_p, c_void_p, c_void_p, c_void_p]
+PEM_read_bio_RSAPrivateKey.restype = c_void_p
+
+#int RSA_private_encrypt(int flen, unsigned char *from,
+#    unsigned char *to, RSA *rsa,int padding);
+RSA_private_encrypt = _eay.RSA_private_encrypt
+RSA_private_encrypt.argtypes = [c_int, c_void_p, c_void_p, c_void_p, c_int]
+RSA_private_encrypt.restype = c_int
+
+RSA_PKCS1_PADDING = 1
+RSA_NO_PADDING = 3
+
+# int RSA_size(const RSA *rsa);
+RSA_size = _eay.RSA_size
+RSA_size.argtypes = [c_void_p]
+RSA_size.restype = c_int
+
+# void RSA_free(RSA *rsa);
+RSA_free = _eay.RSA_free
+RSA_free.argtypes = [c_void_p]
+
+class Key(object):
+    """An OpenSSL RSA private key."""
+    
+    def __init__(self, fp):
+        if isinstance(fp, basestring):
+            fp = open(fp, 'rb')
+        self.raw = fp.read()
+        self.key = None
+        self._load_key()
+        
+    def _load_key(self):
+        if '\0' in self.raw:
+            # Raw string has embedded nulls, treat it as binary data
+            buf = create_string_buffer(self.raw, len(self.raw))
+        else:
+            buf = create_string_buffer(self.raw)
+        
+        bio = BIO_new_mem_buf(buf, len(buf))
+        try:
+            self.key = PEM_read_bio_RSAPrivateKey(bio, 0, 0, 0)
+            if not self.key:
+                raise SSLError('Unable to load RSA private key')
+        finally:
+            BIO_free(bio)
+    
+    def encrypt(self, value, padding=RSA_PKCS1_PADDING):
+        buf = create_string_buffer(value, len(value))
+        size = RSA_size(self.key)
+        output = create_string_buffer(size)
+        ret = RSA_private_encrypt(len(buf), buf, output, self.key, padding)
+        if ret == 0:
+            raise SSLError('Unable to encrypt data')
+        return output.raw[:ret]
+    
+    def __del__(self):
+        if self.key:
+            RSA_free(self.key)

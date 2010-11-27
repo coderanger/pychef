4742e3d2ed59d4e3da5404671b4c0ad49b70c4fa
Noah Kantrowitz <noah@coderanger.net>
2010-11-26 19:35:38 -0800

    Basic shell for PyChef.

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'PyChef',
+    version = '0.1',
+    packages = find_packages(),
+    author = 'Noah Kantrowitz',
+    author_email = 'noah@coderanger.net',
+    description = 'Python implementation of a Chef API client.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    license = 'BSD',
+    keywords = '',
+    url = 'http://github.com/coderanger/pychef',
+    classifiers = [
+        #'Development Status :: 1 - Planning',
+        #'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        #'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        #'Development Status :: 7 - Inactive',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    zip_safe = False,
+)

9b70efa689f5ca5f89917afe440300cf32ed4cc7
Noah Kantrowitz <noah@coderanger.net>
2010-11-29 19:23:23 -0800

    Forgot to fix this.

--- chef/__init__.py
+++ chef/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2010 Noah Kantrowitz <noah@coderanger.net>
-from chef.api import ChefAPI, autodiscover
+from chef.api import ChefAPI, autoconfigure
 from chef.exceptions import ChefError
 from chef.node import Node

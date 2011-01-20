4169f60ce8fa6666b28a1129845c816889a6459a
Noah Kantrowitz <noah@coderanger.net>
2011-01-20 00:14:00 -0800

    Add a system for tests to register objects for deletion.
    They should be deleted no matter the outcome of
    the test.

--- chef/tests/__init__.py
+++ chef/tests/__init__.py
@@ -18,6 +18,18 @@ class ChefTestCase(TestCase):
         super(ChefTestCase, self).setUp()
         self.api = test_chef_api()
         self.api.set_default()
+        self.objects = []
+
+    def tearDown(self):
+        for obj in self.objects:
+            try:
+                obj.delete()
+            except ChefError, e:
+                print e
+                # Continue running
+
+    def register(self, obj):
+        self.objects.append(obj)
 
     def random(self, length=8, alphabet='0123456789abcdef'):
         return ''.join(random.choice(alphabet) for _ in xrange(length))

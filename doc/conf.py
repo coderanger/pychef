fa958a6a8678502d57a27d1267f39abe303eb08a
Noah Kantrowitz <noah@coderanger.net>
2011-02-02 21:09:11 -0800

    Update copyright.

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyChef'
-copyright = u'2010, Noah Kantrowitz'
+copyright = u'2010-2011, Noah Kantrowitz'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

88cef988075adcb22dc0b89525cedceb75bdafbb
Noah Kantrowitz <noah@coderanger.net>
2011-02-07 22:47:54 -0800

    Refactor tests a bit.

--- chef/tests/test_rsa.py
+++ chef/tests/test_rsa.py
@@ -6,22 +6,30 @@ from chef.rsa import Key, SSLError
 from chef.tests import TEST_ROOT
 
 class RSATestCase(unittest2.TestCase):
-    def setUp(self):
-        self.key = Key(os.path.join(TEST_ROOT, 'client.pem'))
-        self.pubkey = Key(os.path.join(TEST_ROOT, 'client_pub.pem'))
+    def test_load_private(self):
+        key = Key(os.path.join(TEST_ROOT, 'client.pem'))
+        self.assertFalse(key.public)
+
+    def test_load_public(self):
+        key = Key(os.path.join(TEST_ROOT, 'client_pub.pem'))
+        self.assertTrue(key.public)
 
     def test_private_export(self):
+        key = Key(os.path.join(TEST_ROOT, 'client.pem'))
         raw = open(os.path.join(TEST_ROOT, 'client.pem'), 'rb').read()
-        self.assertTrue(self.key.private_export().strip(), raw.strip())
+        self.assertTrue(key.private_export().strip(), raw.strip())
 
     def test_public_export(self):
+        key = Key(os.path.join(TEST_ROOT, 'client.pem'))
         raw = open(os.path.join(TEST_ROOT, 'client_pub.pem'), 'rb').read()
-        self.assertTrue(self.key.public_export().strip(), raw.strip())
+        self.assertTrue(key.public_export().strip(), raw.strip())
 
     def test_private_export_pubkey(self):
+        key = Key(os.path.join(TEST_ROOT, 'client_pub.pem'))
         with self.assertRaises(SSLError):
-            self.pubkey.private_export()
+            key.private_export()
 
     def test_public_export_pubkey(self):
+        key = Key(os.path.join(TEST_ROOT, 'client_pub.pem'))
         raw = open(os.path.join(TEST_ROOT, 'client_pub.pem'), 'rb').read()
-        self.assertTrue(self.pubkey.public_export().strip(), raw.strip())
+        self.assertTrue(key.public_export().strip(), raw.strip())

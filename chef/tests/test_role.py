9fece51bc6b3496381871c0fc7db486f8fbfebd7
Noah Kantrowitz <noah@coderanger.net>
2011-02-20 13:24:44 -0800

    Add tests for role attributes.

--- chef/tests/test_role.py
+++ chef/tests/test_role.py
@@ -8,18 +8,26 @@ class RoleTestCase(ChefTestCase):
         self.assertTrue(r.exists)
         self.assertEqual(r.description, 'Static test role 1')
         self.assertEqual(r.run_list, [])
+        self.assertEqual(r.default_attributes['test_attr'], 'default')
+        self.assertEqual(r.default_attributes['nested']['nested_attr'], 1)
+        self.assertEqual(r.override_attributes['test_attr'], 'override')
 
     def test_create(self):
         name = self.random()
-        r = Role.create(name, description='A test role', run_list=['recipe[foo]'])
+        r = Role.create(name, description='A test role', run_list=['recipe[foo]'],
+                        default_attributes={'attr': 'foo'}, override_attributes={'attr': 'bar'})
         self.register(r)
         self.assertEqual(r.description, 'A test role')
         self.assertEqual(r.run_list, ['recipe[foo]'])
+        self.assertEqual(r.default_attributes['attr'], 'foo')
+        self.assertEqual(r.override_attributes['attr'], 'bar')
 
         r2 = Role(name)
         self.assertTrue(r2.exists)
         self.assertEqual(r2.description, 'A test role')
         self.assertEqual(r2.run_list, ['recipe[foo]'])
+        self.assertEqual(r2.default_attributes['attr'], 'foo')
+        self.assertEqual(r2.override_attributes['attr'], 'bar')
 
     def test_delete(self):
         name = self.random()

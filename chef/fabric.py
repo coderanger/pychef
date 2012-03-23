e217e10639f34153750e5459729b93f0d6c0c086
Noah Kantrowitz <noah@coderanger.net>
2012-03-22 23:03:19 -0700

    Improvements to the fabric integration layer.
    The defaults now work nicely with The Cloud. The change to callables isn't backwards compatible, but the prior behavior was never a part of a release so I'm not too worried about it.

--- chef/fabric.py
+++ chef/fabric.py
@@ -4,15 +4,13 @@ from chef.exceptions import ChefError, ChefAPIVersionError
 from chef.search import Search
 
 class Roledef(object):
-    """Represents a Fabric roledef for a Chef role.
-        
-    hostname_attr can either be a string that is the attribute in the chef node that holds the real hostname.
-    Or it can be a function that takes a Node object as a parameter and returns the attribute.
-    """ 
+    """Represents a Fabric roledef for a Chef role."""
     def __init__(self, name, api, hostname_attr, environment=None):
         self.name = name
         self.api = api
         self.hostname_attr = hostname_attr
+        if isinstance(self.hostname_attr, basestring):
+            self.hostname_attr = (self.hostname_attr,)
         self.environment = environment
 
     def __call__(self):
@@ -21,14 +19,23 @@ class Roledef(object):
             query += ' AND chef_environment:%s' % self.environment
         for row in Search('node', query, api=self.api):
             if row:
-                attr = self.hostname_attr(row.object) if callable(self.hostname_attr) else self.hostname_attr
-                yield row.object.attributes.get_dotted(attr)
+                if callable(self.hostname_attr):
+                    yield self.hostname_attr(row.object)
+                else:
+                    for attr in self.hostname_attr:
+                        try:
+                            yield row.object.attributes.get_dotted(attr)
+                            break
+                        except KeyError:
+                            continue
+                    else:
+                        raise ValueError('Cannot find a usable hostname attribute for node %s', row.object)
 
 
-def chef_roledefs(api=None, hostname_attr = 'fqdn', environment = '_default'):
+def chef_roledefs(api=None, hostname_attr=['cloud.public_hostname', 'fqdn'], environment='_default'):
     """Build a Fabric roledef dictionary from a Chef server.
 
-    Example:
+    Example::
 
         from fabric.api import env, run, roles
         from chef.fabric import chef_roledefs
@@ -39,21 +46,21 @@ def chef_roledefs(api=None, hostname_attr = 'fqdn', environment = '_default'):
         def mytask():
             run('uptime')
 
-    hostname_attr can either be a string that is the attribute in the chef node that holds the real hostname.
-    Or it can be a function that takes a Node object as a parameter and returns the attribute.
+    ``hostname_attr`` can either be a string that is the attribute in the chef
+    node that holds the hostname or IP to connect to, an array of such keys to
+    check in order (the first which exists will be used), or a callable which
+    takes a :class:`~chef.Node` and returns the hostname or IP to connect to.
     
     To refer to a nested attribute, separate the levels with '.' e.g. 'ec2.public_hostname'
 
-    For example:
-        def use_ec2_hostname(node):
-            if node.attributes.has_dotted('fqdn'):
-              return 'fqdn'
-            else:
-              return 'ec2.public_hostname'
-      
-        env.roledefs = chef_roledefs(hostname_attr = use_ec2_hostname)
+    ``environment`` is the chef environment whose nodes will be fetched. The
+    default environment is '_default'.
 
-    environment is the chef environment whose nodes will be fetched. The default environment is "_default".
+    .. versionadded:: 0.1
+
+    .. versionadded:: 0.2
+        Support for iterable and callable values for  the``hostname_attr``
+        argument, and the ``environment`` argument.
     """
     api = api or ChefAPI.get_global() or autoconfigure()
     if not api:
